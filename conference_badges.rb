81cf8f8a0f629ee42a40d8ccda128bbda161eae4
Raúl Reymundi <raul@kickstarter.com>
2022-09-25 22:00:50 -0400

    commit tree eb4ef42021196c7d449644f830bfaa0545e0cd1e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map { |name| "Hello, my name is #{name}." }
+end
+
+def assign_rooms(array)
+    array.map.with_index(1) { |person, room| "Hello, #{person}! You'll be assigned to room #{room}!" }
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |badge|
+      puts badge
+    end
+  
+    assign_rooms(array).each do |room|
+      puts room
+    end
+  end
